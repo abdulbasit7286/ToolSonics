2d664e2c74b3d714c9784716eb1a87c32b1a58ba
Abdul Basit <abdulbasit983449@gmail.com>
2025-12-15 19:53:58 +0530

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const openBtn = document.getElementById("openMobile");
+  const closeBtn = document.getElementById("closeMobile");
+  const menu = document.getElementById("mobileMenu");
+
+  if (!openBtn || !closeBtn || !menu) return;
+
+  openBtn.addEventListener("click", () => {
+    menu.style.display = "block";
+    document.body.style.overflow = "hidden";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    menu.style.display = "none";
+    document.body.style.overflow = "auto";
+  });
+});
 
